53f3eb734fe4dfccbce602b3dd19ee06e0850c44
Alan Coopersmith <alan.coopersmith@oracle.com>
2010-10-30 09:13:47 -0700

    Sun's copyrights now belong to Oracle
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/sun_kbd.c
+++ src/sun_kbd.c
@@ -23,7 +23,7 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 /*
- * Copyright © 2004-2009 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright (c) 2004-2009, Oracle and/or its affiliates. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
