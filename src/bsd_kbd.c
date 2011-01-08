78652023a9f7758635e5507255826d79fbb2b028
Alexandr Shadchin <alexandr.shadchin@gmail.com>
2011-01-08 17:09:38 +0500

    bsd: Replace deprecated xfree/xcalloc with free & calloc
    Signed-off-by: Alexandr Shadchin <Alexandr.Shadchin@gmail.com>
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/bsd_kbd.c
+++ src/bsd_kbd.c
@@ -354,11 +354,11 @@ OpenKeyboard(InputInfoPtr pInfo)
 #endif
         default:
            xf86Msg(X_ERROR,"\"%s\" is not a valid keyboard protocol name\n", s);
-           xfree(s);
+           free(s);
            return FALSE;
     }
     xf86Msg(X_CONFIG, "%s: Protocol: %s\n", pInfo->name, s);
-    xfree(s);
+    free(s);
 
     s = xf86SetStrOption(pInfo->options, "Device", NULL);
     if (s == NULL) {
@@ -375,12 +375,12 @@ OpenKeyboard(InputInfoPtr pInfo)
 	pInfo->fd = open(s, O_RDONLY | O_NONBLOCK | O_EXCL);
        if (pInfo->fd == -1) {
            xf86Msg(X_ERROR, "%s: cannot open \"%s\"\n", pInfo->name, s);
-           xfree(s);
+           free(s);
            return FALSE;
        }
        pKbd->isConsole = FALSE;
        pKbd->consType = xf86Info.consType;
-       xfree(s);
+       free(s);
     }
 
 #ifdef WSCONS_SUPPORT
@@ -445,8 +445,8 @@ xf86OSKbdPreInit(InputInfoPtr pInfo)
 
     pKbd->OpenKeyboard = OpenKeyboard;
     pKbd->CustomKeycodes = FALSE;
-    
-    pKbd->private = xcalloc(sizeof(BsdKbdPrivRec), 1);
+
+    pKbd->private = calloc(sizeof(BsdKbdPrivRec), 1);
     if (pKbd->private == NULL) {
        xf86Msg(X_ERROR,"can't allocate keyboard OS private data\n");
        return FALSE;
